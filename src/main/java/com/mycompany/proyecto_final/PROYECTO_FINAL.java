640e48a828d69d3de378b5a5095b4ab95fa4e11d
Valeria Bernal Salcedo <vabersal05@gmail.com>
2025-05-19 08:19:52 -0600

    first commit

---
+++ src/main/java/com/mycompany/proyecto_final/PROYECTO_FINAL.java
@@ -0,0 +1,17 @@
+// PROYECTO FINAL 8-PUZZLE
+// Valeria Bernal Salcedo
+// Fernando Gabriel Hernandez Junco ID: 522266
+// Rosa Lizeth Suarez Medina ID: 302307
+
+package com.mycompany.proyecto_final;
+
+public class PROYECTO_FINAL {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new INICIO().setVisible(true);
+            }
+        });
+    }
+}
